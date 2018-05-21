33148da16f076652bb763ed0aa565016567a55ca
Amartya Bose <amartya.bose@gmail.com>
2018-05-21 10:48:21 -0500

    Basic setup done. Debugging + code restructuring left. Split into files.

--- src/pimc.cxx
+++ src/pimc.cxx
@@ -10,8 +10,10 @@
 #include <boost/property_tree/xml_parser.hpp>
 #include <boost/property_tree/ptree.hpp>
 #include <boost/foreach.hpp>
+#include <boost/shared_ptr.hpp>
 namespace bRand = boost::random;
 namespace pt = boost::property_tree;
+using boost::shared_ptr;
 
 bRand::mt19937 generator;
 
@@ -33,15 +35,23 @@ inline double random_normal(double mean, double sigma) {
 struct Parameters {
     unsigned ndimensions, natoms, nbeads;
     double beta;
+    unsigned nBrownianBridge, nTranslateLength;
+    unsigned nMC, nblocks;
+    unsigned prop_level;
     void load(const char *fname) {
         pt::ptree tree;
         read_xml(fname, tree);
+        prop_level = tree.get<unsigned>("pimc.propagator", 2);
         BOOST_FOREACH (pt::ptree::value_type const &v, tree.get_child("pimc")) {
             if(v.first == "parameters") {
                 beta = v.second.get<double>("beta");
-                ndimensions = v.second.get<double>("num_dimensions");
-                natoms = v.second.get<double>("num_atoms");
-                nbeads = v.second.get<double>("num_beads");
+                ndimensions = v.second.get<unsigned>("num_dimensions");
+                natoms = v.second.get<unsigned>("num_atoms");
+                nbeads = v.second.get<unsigned>("num_beads");
+                nBrownianBridge = v.second.get<unsigned>("num_brownian_bridge");
+                nTranslateLength = v.second.get<double>("num_translate_length");
+                nMC = v.second.get<unsigned>("num_MC");
+                nblocks = v.second.get<unsigned>("num_blocks");
             }
         }
     }
@@ -74,39 +84,50 @@ struct Configuration {
     arma::mat time_slice(unsigned time_ind) const {
         return positions(arma::span::all, arma::span(time_ind), arma::span::all);
     }
+    arma::mat CoM() const {
+        return arma::mean(positions, 1);
+    }
 };
 
 template <typename PotFunc>
 struct Propagator {
     PotFunc V;
     double tau;
+    Propagator(PotFunc pot, Parameters param) {
+        V = pot;
+        tau = param.beta/param.nbeads;
+    }
     virtual double operator()(Configuration conf) = 0;
 };
 
 template <typename PotFunc>
 struct G2 : public Propagator<PotFunc> {
+    G2(PotFunc pot, Parameters param) : Propagator<PotFunc>(pot, param) {}
     double operator()(Configuration conf) {
         double total_pot = 0;
         for(unsigned t=0; t<conf.num_beads(); t++)
             total_pot += this->V(conf.time_slice(t));
-        return std::exp(total_pot * this->tau);
+        return std::exp(-total_pot * this->tau);
     }
 };
 
 template <typename PotFunc>
 struct G4 : public Propagator<PotFunc> {
+    G4(PotFunc pot, Parameters param) : Propagator<PotFunc>(pot, param) {}
     double operator()(Configuration conf) {
         double total_amplitude = 1;
-        for(unsigned t=0; t<conf.num_beads(); t+=2)
+        for(unsigned t=0; t<conf.num_beads()-2; t+=2) {
             total_amplitude *=
                 4./3. * std::exp(-this->tau/2. * this->V(conf.time_slice(t) - this->tau * this->V(conf.time_slice(t+1)) - this->tau/2. * this->V(conf.time_slice(t+2))))
                 - 1./3. * std::exp(-this->tau * (this->V(conf.time_slice(t)) + this->V(conf.time_slice(t+2))));
+        }
         return total_amplitude;
     }
 };
 
 template <typename PotFunc>
 struct G6 : public Propagator<PotFunc> {
+    G6(PotFunc pot, Parameters param) : Propagator<PotFunc>(pot, param) {}
     double operator()(Configuration conf) {
         double total_amplitude = 1;
         for(unsigned t=0; t<conf.num_beads(); t+=4)
@@ -120,6 +141,7 @@ struct G6 : public Propagator<PotFunc> {
 
 template <typename PotFunc>
 struct G8 : public Propagator<PotFunc> {
+    G8(PotFunc pot, Parameters param) : Propagator<PotFunc>(pot, param) {}
     double operator()(Configuration conf) {
         double total_amplitude = 1;
         for(unsigned t=0; t<conf.num_beads(); t+=6)
@@ -132,6 +154,15 @@ struct G8 : public Propagator<PotFunc> {
     }
 };
 
+struct HarmonicOscillator {
+    double omega;
+    HarmonicOscillator() {}
+    HarmonicOscillator(double w) : omega(w) {}
+    double operator()(arma::mat x) {
+        return 0.5 * omega * omega * arma::accu(x % x);
+    }
+};
+
 struct Moves {
     virtual Configuration operator()(Configuration conf, unsigned atom_num) = 0;
 };
@@ -139,7 +170,7 @@ struct Moves {
 struct Translate : Moves {
     arma::mat max_step;
     Translate(Parameters param) {
-        max_step = arma::ones<arma::mat>(param.natoms, param.ndimensions);
+        max_step = param.nTranslateLength*arma::ones<arma::mat>(param.natoms, param.ndimensions);
     }
     virtual Configuration operator()(Configuration conf, unsigned atom_num) {
         arma::vec step(max_step.n_cols);
@@ -155,18 +186,19 @@ struct Translate : Moves {
 struct BrownianBridge : Moves {
     unsigned num_beads_moved;
     double tau;
-    BrownianBridge(unsigned nbeads_move, Parameters param) {
-        num_beads_moved = nbeads_move;
+    BrownianBridge(Parameters param) {
+        num_beads_moved = param.nBrownianBridge;
         tau = param.beta/param.nbeads;
     }
     virtual Configuration operator()(Configuration conf, unsigned atom_num) {
         unsigned nbeads = conf.num_beads();
-        unsigned start = random_integer(0, nbeads);
+        unsigned start = random_integer(0, nbeads-1);
         unsigned end = (start + num_beads_moved + 1) % nbeads;
-        for(int b = start+1, nmoved=0; nmoved < num_beads_moved; b = (b+1)%nbeads, nmoved++) {
-            unsigned L = num_beads_moved - nmoved;
-            for(unsigned d=0; d<conf.num_dims(); d++)
-                conf.positions(atom_num, b, d) = random_normal((conf.positions(atom_num, (b-1)%nbeads, d) + (L-1.)*conf.positions(atom_num, end, d))/L, std::sqrt((L-1.)/L)*tau);
+        for(unsigned b = (start+1)%nbeads, nmoved=0; nmoved < num_beads_moved; b = (b+1)%nbeads, nmoved++) {
+            unsigned L = num_beads_moved - nmoved + 1;
+            for(unsigned d=0; d<conf.num_dims(); d++) {
+                conf.positions(atom_num, b, d) = random_normal((conf.positions(atom_num, (nbeads+b-1)%nbeads, d) + (L-1.)*conf.positions(atom_num, end, d))/L, std::sqrt((L-1.)/L*tau));
+            }
         }
         return conf;
     }
@@ -179,13 +211,46 @@ int main(int argc, char **argv) {
     }
     params.load(argv[1]);
     Configuration c(params);
-    arma::mat necklace0 = c.necklace(0);
-    BrownianBridge bb(5, params);
-    arma::mat necklacebb = bb(c, 0).necklace(0);
-    (necklace0 - necklacebb).print("change?");
-    Translate translate(params);
-    translate(c, 0);
-    arma::mat necklace_trans = translate(c, 0).necklace(0);
-    (necklace0 - necklace_trans).print("change?");
+    HarmonicOscillator osc(2);
+    shared_ptr<Propagator<HarmonicOscillator> > mep;
+    switch(params.prop_level) {
+    case 2:
+        mep.reset(new G2<HarmonicOscillator>(osc, params));
+        break;
+    case 4:
+        mep.reset(new G4<HarmonicOscillator>(osc, params));
+        break;
+    case 6:
+        mep.reset(new G6<HarmonicOscillator>(osc, params));
+        break;
+    case 8:
+        mep.reset(new G8<HarmonicOscillator>(osc, params));
+        break;
+    default:
+        std::cout<<"Not a valid propagator"<<std::endl;
+        exit(1);
+    }
+    std::vector<shared_ptr<Moves> > moves;
+    moves.push_back(shared_ptr<Moves>(new Translate(params)));
+    moves.push_back(shared_ptr<Moves>(new BrownianBridge(params)));
+    std::ofstream ofs("test");
+    arma::vec moves_accepted = arma::zeros<arma::vec>(moves.size());
+    arma::vec xsq = arma::zeros<arma::vec>(params.nblocks);
+    for(unsigned b=0; b<params.nblocks; b++)
+        for(unsigned i=0; i<params.nMC/params.nblocks; i++) {
+            for(unsigned m=0; m<moves.size(); m++)
+                for(unsigned atom=0; atom<c.num_atoms(); atom++) {
+                    Configuration cnew = (*moves[m])(c, atom);
+                    if((*mep)(cnew)/(*mep)(c)>random_float(0,1)) {
+                        c = cnew;
+                        moves_accepted(m)++;
+                    }
+                }
+            xsq(b) += c.time_slice(0)(0)*c.time_slice(0)(0);
+            ofs<<c.time_slice(0).st()<<std::endl;
+        }
+    xsq /= params.nMC/params.nblocks;
+    std::cout<<arma::mean(xsq)<<'\t'<<arma::stddev(xsq)<<std::endl;
+    (moves_accepted/params.nMC).print("Fraction of moves accepted");
     return 0;
 }
